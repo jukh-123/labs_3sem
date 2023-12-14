a7091ffe8dd4290220721e04d9752d443a876e4e
jukh <jukh@tutamail.com>
2023-12-14 22:25:40 +0100

    5lb atasd

--- ALaSD/lab_6/lb6_1.cpp
+++ ALaSD/lab_6/lb6_1.cpp
@@ -16,19 +16,22 @@
 map<string, unsigned short int> cities;
 map<unsigned short int, string> cities_idx;
 unsigned short int city_id = 0;
-// class City
-// {
-// public:
-//     string city_name = "";
-//     unsigned short int city_id;
-//     string country = "";
-//     City(string city_name, string country)
-//     {
-//         this->city_name = city_name;
-//         this->city_id = city_id++;
-//         this->country = country;
-//     }
-// };
+const int INF = 1e9;
+
+class Route
+{
+public:
+    Route(string start, string end, int price)
+    {
+        this->start = start;
+        this->end = end;
+        this->price = price;
+    }
+    Route(){;}
+    string start, end;
+    int price;
+    
+};
 
 /*Задача 2:
 А. Компанія забезпечує інтернет-підключення користувачів за
@@ -45,51 +48,61 @@ unsigned short int city_id = 0;
 магістральними лініями, визначити всі найкоротші шляхи всередині
 графу.*/
 
-vector<vector<int>> fill_prices(vector<vector<int>> routes)
+vector<int> get_cheapest(vector<Route> routes, string to_start)
 {
-    routes[cities["New York"]][cities["California"]] = 100;
-    routes[cities["California"]][cities["New York"]] = 120;
+    vector<int> cheapest_prices(city_id, INF);
+    vector<int> from(city_id, -1);
+    int start = cities[to_start];
 
-    routes[cities["Houston"]][cities["Washington"]] = 150;
-    routes[cities["Washington"]][cities["Houston"]] = 130;
+    cheapest_prices[start] = 0; // визначаємо ціну до стартового міста як 0
 
-    routes[cities["Toronto"]][cities["Montreal"]] = 10;
-    routes[cities["Montreal"]][cities["Toronto"]] = 7;
+    for (size_t i = 0; i < city_id - 1; i++) // v - 1 ітерацій
+    {
+        for(auto &[start1, end, price] : routes) // у кожній ітерації розглядаємо всі існуючі шляхи
+        { // перевірка на те, що ціна до початку маршруту не є нескінченністю, інакше може бути ситуація коли віднімаємо від нескінченності
+            if(cheapest_prices[cities[start1]] != INF && cheapest_prices[cities[end]] > cheapest_prices[cities[start1]] + price)
+            { // намагаємось покращити відстань до кожного ребра
+                cheapest_prices[cities[end]] = cheapest_prices[cities[start1]] + price; 
+                from[cities[end]] = cities[start1]; // у from лежать останній(найоптимальніший) варіант як попасти у місто
+            }
+        }
+    }
 
-    routes[cities["Ottawa"]][cities["Montreal"]] = 12;
-    routes[cities["Montreal"]][cities["Ottawa"]] = 10;
+    vector<int> path;
+    string finish;
 
-    routes[cities["Mexico City"]][cities["California"]] = -5;
-    routes[cities["California"]][cities["Mexico City"]] = -7;
+    cout << "\n\nМісто призначення: ";
+    cin >> finish;
+    for (int v = cities[finish]; v != -1; v = from[v])
+    { // записуємо як ми прийшли у кожне місто з маршруту востаннє
+        path.push_back(v);
+    }
+    reverse(path.begin(), path.end());
+    
 
-    routes[cities["Guadalajara"]][cities["Miami"]] = 34;
-    routes[cities["Miami"]][cities["Guadalajara"]] = 55;
+    cout << "Найдешевший маршрут у " << cities_idx[cities[finish]] << 
+    " коштує " << cheapest_prices[cities[finish]] <<
+    " по містам: ";
+    for (size_t j = 0; j < path.size(); j++)
+    {
+        cout << cities_idx[path[j]] << " - ";
+    }
 
-    routes[cities["New York"]][cities["Washington"]] = 15;
-    routes[cities["Washington"]][cities["New York"]] = 15;
+    
 
-    routes[cities["Toronto"]][cities["New York"]] = 12;
-    routes[cities["New York"]][cities["Toronto"]] = 12;
-
-    routes[cities["Miami"]][cities["California"]] = 25;
-    routes[cities["California"]][cities["Miami"]] = 25;
-
-    routes[cities["Washington"]][cities["Houston"]] = 31;
-    routes[cities["Houston"]][cities["Washington"]] = 29;
-
-    return routes;
+    return cheapest_prices;
 }
 
 void insert_cities()
 {
-    cities.insert(make_pair( "New York", city_id++));
+    cities.insert(make_pair( "New-York", city_id++));
     cities.insert(make_pair( "California", city_id++));
     cities.insert(make_pair( "Houston", city_id++));
     cities.insert(make_pair( "Washington", city_id++));
     cities.insert(make_pair( "Toronto", city_id++));
     cities.insert(make_pair( "Montreal", city_id++));
     cities.insert(make_pair( "Ottawa", city_id++));
-    cities.insert(make_pair( "Mexico City", city_id++));
+    cities.insert(make_pair( "Mexico-City", city_id++));
     cities.insert(make_pair( "Guadalajara", city_id++));
     cities.insert(make_pair( "Miami", city_id++));
     for (auto &i : cities)
@@ -102,72 +115,24 @@ void insert_cities()
 int main(int argc, char const *argv[])
 {
     insert_cities();
-    vector<vector<int>> routes(city_id);
-    for (size_t i = 0; i < city_id; i++)
+
+    vector<Route> routes(21);
+    for (auto &[start, end, price]: routes)
     {
-        routes[i].resize(city_id);
+        cin >> start >> end >> price;
     }
-    routes = fill_prices(routes);
     
     string start_city;
-    cout << "Введіть назву міста: "; cin >> start_city;
+    cout << "\n\nПочаткове місто: ";
+    cin >> start_city;
     cout << endl;
 
     cout << "Індекс міста: "<< cities[start_city] << endl;
     const int INF = 1e9;
-    vector<vector<int>> cheapest_routes(city_id);
-    for (size_t i = 0; i < city_id; i++)
-    {
-        cheapest_routes[i].resize(city_id, INF);
-    }
-    cheapest_routes[cities[start_city]][cities[start_city]] = 0;
-    for (size_t i = 0; i < city_id - 1; i++)
-    {
-        for (size_t j = 0; j < routes.size(); j++)
-        {
-            for (size_t k = 0; k < routes[i].size(); k++)
-            {
-                cheapest_routes[j][k] = min(cheapest_routes[j][k], routes[j][k]);
-            }
-        }
-    }
-    
-    
+   
+    vector<int> cheapest = get_cheapest(routes, start_city);
 
-    for (size_t i = 0; i < city_id; i++)
-    {
-        for (size_t j = 0; j < cheapest_routes[i].size(); j++)
-        {
-            if(j != i)
-            {
-                cout << "Route: ";
-                for (auto &k : cities)
-                {
-                    if(k.second == i)
-                    {
-                        cout << k.first << " to ";
-                        break;
-                    }
-                }
-                for (auto &k : cities)
-                {
-                    if(k.second == j)
-                    {
-                        cout << k.first << " --- ";
-                        break;
-                    }
-                }
-            
-                cout << cheapest_routes[i][j] << endl;
-            }
-            
-        }
-        cout << endl;
-        
-    }
     
-
-
     
     return 0;
 }
